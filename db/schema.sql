8a86fe2f0f074613b6556703a65f9d1ba6aabd77
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-22 16:23:32 -0600

    creating the role table values

--- db/schema.sql
+++ db/schema.sql
@@ -6,10 +6,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 -- creating the department table
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- we're declaring the variable 'id', it must be an integer, the NOT NULL statement dictates it cannot be null, PRIMARY KEY identifies each record in a table.
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- we're declaring the variable 'id', it must be an integer, the NOT NULL statement dictates it cannot be null, PRIMARY KEY identifies each record in a table. AUTO_INCREMENT assigns a unique primary key to every inserted record.
     name VARCHAR(30) NOT NULL -- declaring the variable 'name', we're giving it a max of 30 varying characters and telling it that it can't be a null value.
 );
 -- creating a sub-category role table within our database.
 CREATE TABLE role (
-    id
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- we're declaring the variable 'id', it must be an integer, the NOT NULL statement dictates it cannot be null, PRIMARY KEY identifies each record in a table. AUTO_INCREMENT assigns a unique primary key to every inserted record.
+    title VARCHAR(30) NOT NULL, -- declaring the variable title, setting a max of 30 varying characters and telling SQL to only allow object values. if not it won't function.
+    salary DECIMAL(10, 2) NOT NULL, -- we're declare a salary variable, allowing a decimal number to pass to the table with a whole number value of 10 figures and a decimal place of 2.
+    
 )
